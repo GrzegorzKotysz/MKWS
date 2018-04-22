e1fe6431e3dea94765929c48691867286edd9636
Grzegorz Kotysz <grzegorzkotysz@gmail.com>
2018-04-22 16:54:48 +0200

    Updated script and tex file Script now created up to the point of editing thermophysicalProperties file. Problem with python script resolved (directory must be changed in bash script, the location of python script does not matter. It searches the current directory for file).

--- ISAT-CK7.sh
+++ ISAT-CK7.sh
@@ -1,13 +1,16 @@
 # script for creating and running tutorial case
 # you have to be inside OpenFOAM bash before running this script
+# place all python scripts in the same folder as this shell script
 # DRM19.dat and thermo30.dat files have to be downloaded
 # you will be prompted to specify their directory
+
+# if scripts directory will be needed for copying python scripts etc
+scriptsDirectory="$PWD"
 
 echo copying tutorial case
 cp -r $FOAM_TUTORIALS/combustion/reactingFoam/laminar/counterFlowFlame2D $WM_PROJECT_USER_DIR/run/counterFlowFlame2D
 echo creating chemkin directory in counterFlowFlame2D
-cd $WM_PROJECT_USER_DIR/run/counterFlowFlame2D
-mkdir chemkin
+mkdir $WM_PROJECT_USER_DIR/run/counterFlowFlame2D/chemkin
 
 while :
 do
@@ -20,6 +23,9 @@ do
         if [[ -e $WM_PROJECT_USER_DIR/drm19.dat && -e $WM_PROJECT_USER_DIR/thermo30.dat ]]
         then
             echo directory contains needed files. Proceeding.
+            echo copying drm19.dat and thermo30.dat to case directory
+            cp $WM_PROJECT_USER_DIR/drm19.dat $WM_PROJECT_USER_DIR/run/counterFlowFlame2D/chemkin/
+            cp $WM_PROJECT_USER_DIR/thermo30.dat $WM_PROJECT_USER_DIR/run/counterFlowFlame2D/chemkin/
             break
         else
             echo directory does not contain needed files. Provide appropriate directory.
@@ -28,6 +34,9 @@ do
         if [[ -e $griDirectory/drm19.dat && -e $griDirectory/thermo30.dat ]]
         then
             echo directory contains needed files. Proceeding.
+            echo copying drm19.dat and thermo30.dat to case directory
+            cp $griDirectory/drm19.dat $WM_PROJECT_USER_DIR/run/counterFlowFlame2D/chemkin/
+            cp $griDirectory/thermo30.dat $WM_PROJECT_USER_DIR/run/counterFlowFlame2D/chemkin/
             break
         else
             echo directory does not contain needed files. Provide appropriate directory.
@@ -35,3 +44,22 @@ do
     fi
 
 done
+
+# variable to store case directory - code easier to read
+caseDir="$WM_PROJECT_USER_DIR/run/counterFlowFlame2D"
+echo caseDir = "$caseDir"
+echo making backup of files to be changed
+# cp $caseDir/constant/thermophysicalProperties $caseDir/constant/~thermophysicalProperties
+# rm $caseDir/constant/thermophysicalProperties
+mv $caseDir/constant/thermophysicalProperties $caseDir/constant/~thermophysicalProperties
+
+# solution with shell editing would be probably faster,
+# but for the sake of learning Python was chosen.
+# copying of python scripts now unnecessary as scriptsDirectory variable is used
+# echo copying python scripts
+# cp ./thermophysicalProperties.py $caseDir/constant/thermophysicalProperties.py
+echo editing constant/thermophysicalProperties
+
+# to launch python script in appropriate location cd command is used
+cd $caseDir/constant/
+python $scriptsDirectory/thermophysicalProperties.py
